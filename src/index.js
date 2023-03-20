37fb61ac0fe59db16e51902dd6e363e500183aa4
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-03-20 22:27:54 +0900

    ドラック&ドロップの実装課題終了

--- src/index.js
+++ src/index.js
@@ -2,31 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <DragDropContext>
-        <Droppable droppableId="droppable">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              <Draggable draggableId="item0" index={0}>
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    item0
-                  </div>
-                )}
-              </Draggable>
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </div>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
